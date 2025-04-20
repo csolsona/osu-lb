32fa3a3b4a02ff7523347ca74687e819e9e569f5
Carlos <75030224+csolsona@users.noreply.github.com>
2025-04-21 01:10:20 +0200

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const apiData = ref();
+
+async function fetchData() {
+  const res = await fetch('/api/hello');
+  apiData.value = await res.json();
+}
+</script>
+
+<template>
+  <h1>Frontend Vue + Bun</h1>
+  <button @click="fetchData">API Request</button>
+  <pre v-if="apiData">{{ apiData }}</pre>
+</template>
